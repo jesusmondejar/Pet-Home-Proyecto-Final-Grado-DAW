7c33208e7491eff3965311e0a295c768852dafc9
Jesús Mondéjar <jesusmondejarcollado@gmail.com>
2025-04-16 19:06:31 +0200

    creados los componentes ,enrutamiento y service(falta lo del http). el navbar es una mierda pero habrá que ir diseñando todos los componentes para que se quede chula la pagina. proxima dia prueba con las tarjetas

---
+++ PetHomeAngular/src/app/secciones/sobre-nosotros/sobre-nosotros.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre-nosotros',
+  imports: [],
+  templateUrl: './sobre-nosotros.component.html',
+  styles: ``
+})
+export class SobreNosotrosComponent {
+
+}
