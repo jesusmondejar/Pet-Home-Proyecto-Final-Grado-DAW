344cdeb5d90f54423e326fb0e0d156e40ca92ade
victor <1918967@alu.murciaeduca.es>
2025-04-29 18:55:07 +0200

    añadido fondo y  navMenu

--- PetHomeAngular/src/app/app.component.ts
+++ PetHomeAngular/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeadComponent } from "./head/head.component";
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
+import { FootComponent } from "./foot/foot.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeadComponent, NavMenuComponent, FootComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
