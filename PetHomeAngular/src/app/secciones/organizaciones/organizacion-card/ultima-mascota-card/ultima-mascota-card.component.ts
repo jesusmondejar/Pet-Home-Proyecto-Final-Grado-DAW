455beecab1fd516b465c1065b8774d19375f946a
Jesús Mondéjar <jesusmondejarcollado@gmail.com>
2025-05-09 19:45:05 +0200

    Organizaciones empezado, falta hacer el html bien. Logica creada

---
+++ PetHomeAngular/src/app/secciones/organizaciones/organizacion-card/ultima-mascota-card/ultima-mascota-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ultima-mascota-card',
+  imports: [],
+  templateUrl: './ultima-mascota-card.component.html',
+  styles: ``
+})
+export class UltimaMascotaCardComponent {
+
+}
--- PetHomeAngular/src/app/secciones/organizaciones/organizaciones.component.ts
+++ PetHomeAngular/src/app/secciones/organizaciones/organizaciones.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FuncionesOrganizacionesService } from '../../services/funciones-organizaciones.service';
+import { OrganizacionCardComponent } from './organizacion-card/organizacion-card.component';
 
 @Component({
   selector: 'app-organizaciones',
-  imports: [],
+  imports: [OrganizacionCardComponent],
   templateUrl: './organizaciones.component.html',
-  styles: ``
+  styles: `.hero {
+    position: relative;
+    width: 100%;
+    height: 70vh; /* Alto de la sección héroe */
+    background: url('/assets/img/perros.jpg') no-repeat center center;
+    background-size: cover;
+  }
+  .organizacion-card-section {
+    background-color: white;
+    padding: 2rem;
+    border-radius: 10px;
+    text-align: center;
+    margin: 2rem;
+    box-shadow: 0 0 10px rgba(0,0,0,0.15);
+  }`
 })
 export class OrganizacionesComponent {
 
+  constructor(private funcionesOrganizaciones: FuncionesOrganizacionesService) {
+      this.funcionesOrganizaciones.getOrganizaciones(); // carga datos al inicio
+      // this.organizacionesFiltradas = this.funcionesOrganizaciones.getOrganizacion();  muestra todos por defecto
+     }
+
+
+     getOrganizaciones() {
+      return this.funcionesOrganizaciones.getOrganizacion()
+    }
 }
